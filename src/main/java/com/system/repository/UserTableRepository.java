25930e3b187cdc1afb203d1f566d192e0faa7fe5
alex.ryberg <alex@rybergs.com>
2021-05-06 17:24:08 -0500

    Fixed minor pathing in html files
    Let me know if you'd like for me to run you through my changes

--- src/main/java/com/system/repository/UserTableRepository.java
+++ src/main/java/com/system/repository/UserTableRepository.java
@@ -1,5 +1,10 @@
 package com.system.repository;
 
+/**
+ * @author Alex Ryberg - alexr151
+ * CIS175 - Spring 2021
+ */
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.system.beans.UserTable;
 
